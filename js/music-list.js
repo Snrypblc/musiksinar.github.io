d5d431d725abdd86e2253a2a573c7e564d124c5e
Snrypblc <103938346+Snrypblc@users.noreply.github.com>
2022-08-03 20:33:45 +0700

    Add files via upload

---
+++ js/music-list.js
@@ -0,0 +1,142 @@
+// To add more song, just copy the following code and paste inside the array
+
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+let allMusic = [
+  {
+    name: "Ransom",
+    artist: "Lil Tecca",
+    img: "Lil Tecca  Ransom",
+    src: "Lil Tecca  Ransom"
+  },
+  {
+    name: "Infone Maseh",
+    artist: "Woro Widowati",
+    img: "infone maseh",
+    src: "infone maseh"
+  },
+  {
+    name: "Equality",
+    artist: "Begundal",
+    img: "Equality",
+    src: "Equality"
+  },
+  {
+    name: "Putuskan pacar",
+    artist: "BravesBoy",
+    img: "BravesBoy",
+    src: "BravesBoy"
+  },
+  {
+    name: "Venom",
+    artist: "Eminem",
+    img: "Eminem  Venom",
+    src: "Eminem  Venom"
+  },
+  {
+    name: "Gedang Kluthuk",
+    artist: "Waru Leaf",
+    img: "Gedang Kluthuk",
+    src: "Gedang Kluthuk"
+  },
+  {
+    name: "Glimpse of Us",
+    artist: "Joji",
+    img: "Joji   Glimpse of Us",
+    src: "Joji   Glimpse of Us"
+  },
+  {
+    name: "Narcos",
+    artist: "Migos",
+    img: "Migos  Narcos",
+    src: "Migos  Narcos"
+  },
+  {
+    name: "Colt 45)",
+    artist: "Crazy Rap",
+    img: "Colt 45 Crazy Rap  Afroman Lyrics HD",
+    src: "Colt 45 Crazy Rap  Afroman Lyrics HD"
+  },
+  {
+    name: "MoonLight",
+    artist: "XXXTENTACION",
+    img: "XXXTENTACION  MOONLIGHT",
+    src: "XXXTENTACION  MOONLIGHT"
+  },
+  {
+    name: "Old Town Road' ",
+    artist: "Lil Nas X",
+    img: "Lil Nas X  Old Town Road",
+    src: "Lil Nas X  Old Town Road"
+  },
+  {
+    name: "Narcos",
+    artist: "Dj",
+    img: "DJ TRUMPET",
+    src: "DJ TRUMPET"
+  },
+  {
+    name: "Teri Mitti - Kesari ",
+    artist: " Zee Music Company",
+    img: "music-13",
+    src: "music-13"
+  },
+  {
+    name: "KHAIRIYAT(BONUS TRACK)-CHHICHHORE",
+    artist: "T-Series",
+    img: "music-14",
+    src: "music-14"
+  },
+  {
+    name: "Waka Waka(This Time for Africa)",
+    artist: "Shakira",
+    img: "music-15",
+    src: "music-15"
+  },
+  {
+    name: "Phao-2 Phut Hon(KAIZ Remix)",
+    artist: "Light Night Music",
+    img: "music-16",
+    src: "music-16"
+  },
+  {
+    name: "スパークル - Your name",
+    artist: "Radwimps",
+    img: "music-17",
+    src: "music-17"
+  },
+  {
+    name: "Ed Sheeran - Shape of You",
+    artist: "Ed Sheeran",
+    img: "music-18",
+    src: "music-18"
+  },
+  {
+    name: "Sia - Unstoppable ",
+    artist: "Loku",
+    img: "music-19",
+    src: "music-19"
+  },
+  {
+    name: "Bhaag Milkha Bhaag - Zinda ",
+    artist: "SonyMusicIndiaVEVO",
+    img: "music-20",
+    src: "music-20"
+  },
+
+
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
